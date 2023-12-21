5d606c1dd97782824396da01b5de303934f3b67c
NekoDK <koldim14kol@gmail.com>
2023-12-21 23:02:56 +0600

    add assets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from "./baner.js";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Banner
+          title="Добро пожаловать!"
+          content="Узнайте больше о наших продуктах и услугах."
+          button="Узнать больше"
+          illustration={logo}
+          type="welcome"
+          separator={true}
+      />
     </div>
   );
 }
